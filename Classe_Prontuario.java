e013ddb4ce5afcc382b18c36e07901c19859a0d9
Matheus Queiroz <62029194+amatheusvq@users.noreply.github.com>
2023-08-08 23:15:12 -0300

    Update and rename Prontuario.java to Classe_Prontuario.java

---
+++ Classe_Prontuario.java
@@ -0,0 +1,37 @@
+//import package.paciente;
+//import package.medico;
+//import package.consulta
+
+import java.time.LocalDateTime;
+
+
+public class Prontuario {
+    private String Alergias;
+    private String Sangue;
+    private String Medicacao;
+    private String SinaisVitais;
+
+    public Prontuario(){}
+    public Prontuario(String Alergias, String Sangue, String Medicacao, String SinaisVitais)
+    {
+        this.Alergias = Alergias;
+        this.Sangue = Sangue;
+        this.Medicacao = Medicacao;
+        this.SinaisVitais = SinaisVitais;
+    }
+
+    public void editarProntuario()
+    {
+        System.out.println("Prontuário editado");
+        
+    }
+    public void excluirProntuario()
+    {
+        System.out.println("Prontuário excluído");
+    }
+    public class gerarProntuario{
+        public gerarProntuario(){}
+    }
+    return prontuario;
+}
+

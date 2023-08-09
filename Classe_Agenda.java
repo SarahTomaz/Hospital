661b3662fa03e3f44a7fb695c497e150e5553efd
Matheus Queiroz <62029194+amatheusvq@users.noreply.github.com>
2023-08-08 23:14:41 -0300

    Update and rename Agenda.java to Classe_Agenda.java

---
+++ Classe_Agenda.java
@@ -0,0 +1,41 @@
+//import paciente;
+//import medico;
+
+import java.time.LocalDateTime;
+
+
+public class Agenda {
+    private String NomePaciente;
+    private String NomeMedico;
+    private LocalDateTime HoradaConsulta;
+    private boolean Status;
+
+    public Agenda(){}
+    public Agenda(String NomePaciente, String NomeMedico, LocalDateTime HoradaConsulta, boolean Status)
+    {
+        this.NomePaciente = NomePaciente;
+        this.NomeMedico = NomeMedico;
+        this.HoradaConsulta = HoradaConsulta;
+        this.Status = Status;
+    }
+
+    public boolean cancelarConsulta()
+    {
+        System.out.println("Consulta cancelada");
+        return true;
+    }
+
+
+    public boolean confirmarConsulta(String nomePaciente, String nomeMedico)
+    {
+        System.out.println(nomePaciente + "Consulta confirmada");
+        System.out.println(nomeMedico + "Consulta confirmada");
+        return true;
+    }
+    public boolean confirmarConsulta(LocalDateTime horadaConsulta)
+    {
+        System.out.println(horadaConsulta + "Consulta confirmada");
+        return true;
+    }
+}
+

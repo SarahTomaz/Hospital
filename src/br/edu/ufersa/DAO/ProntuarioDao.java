138553c42b8343f2077f3eec29ed8fcb536a9147
Sarah Tomaz <89924900+SarahTomaz@users.noreply.github.com>
2023-09-18 21:05:04 -0300

    Update ProntuarioDao.java

--- src/br/edu/ufersa/DAO/ProntuarioDao.java
+++ src/br/edu/ufersa/DAO/ProntuarioDao.java
@@ -1,6 +1,5 @@
 package br.edu.ufersa.DAO;
 
-import br.edu.ufersa.model.entity.Paciente;
 import br.edu.ufersa.model.entity.Prontuario;
 
 import java.sql.Connection;
@@ -10,134 +9,101 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProntuarioDao extends BaseDaoImpl<Prontuario>
-{
-    @Override
-    public Long inserir(Prontuario entity)
-    {
-        Connection con = getConnection();
-        String sql = "INSERT INTO prontuario (horario, observacoes) VALUES (?, ?)";
+public class ProntuarioDao extends BaseDaoImpl<Prontuario> {
 
-        try
-        {
-            PreparedStatement ps = con.prepareStatement(sql);
+    private static final String INSERT_SQL = "INSERT INTO prontuario (horario, observacoes) VALUES (?, ?)";
+    private static final String DELETE_SQL = "DELETE FROM prontuario WHERE p_id = ?";
+    private static final String UPDATE_SQL = "UPDATE prontuario SET observacoes = ? WHERE p_id = ?";
+    private static final String SELECT_BY_FIELD_SQL = "SELECT * FROM prontuario WHERE ";
+
+    @Override
+    public Long inserir(Prontuario entity) {
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(INSERT_SQL, PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setDate(1, entity.getData());
             ps.setString(2, entity.getObservacoes());
             ps.execute();
-            ps.close();
 
-            return ProntuarioDao.buscarId(entity);
-        }
-        catch (SQLException e)
-        {
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    return rs.getLong(1);
+                }
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
-            return null;
-        }
-        finally {closeConnection();}
-    }
-
-    public static Long buscarId(Prontuario entity)
-    {
-        Connection con = getConnection();
-
-        try {
-            String sql = "SELECT * FROM prontuario WHERE horario = ?";
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps = con.prepareStatement(sql);
-
-            ps.setDate(1, entity.getData());
-            ps.setString(2, entity.getObservacoes());
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next())
-                return rs.getLong("p_id");
-            else
-                return null;
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
+        } finally {
+            closeConnection();
         }
         return null;
     }
 
     @Override
-    public void deletar(Prontuario entity)
-    {
-        Connection con = getConnection();
-        String sql = "DELETE FROM prontuario WHERE p_id = ?";
-
-        try
-        {
-            PreparedStatement ps = con.prepareStatement(sql);
-
+    public void deletar(Prontuario entity) {
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(DELETE_SQL)) {
             ps.setLong(1, entity.getId());
             ps.execute();
-            ps.close();
-        }
-        catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection();
         }
-        finally {closeConnection();}
     }
 
     @Override
-    public void alterar(Prontuario entity)
-    {
-        Connection con = getConnection();
-        String sql = "UPDATE paciente SET observacoes = ? WHERE p_id = ?";
-
-        try
-        {
-            PreparedStatement ps = con.prepareStatement(sql);
+    public void alterar(Prontuario entity) {
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(UPDATE_SQL)) {
             ps.setString(1, entity.getObservacoes());
             ps.setLong(2, entity.getId());
             ps.execute();
-            ps.close();
-        }
-        catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection();
         }
-        finally {closeConnection();}
+    }
+
+    public Prontuario buscarPorCampo(String campo, String valor) {
+        String sql = SELECT_BY_FIELD_SQL + campo + " = ?";
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, valor);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return createProntuarioFromResultSet(rs);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return null;
     }
 
     @Override
-    public List<Prontuario> listar()
-    {
-        Connection con = getConnection();
-        String sql = "SELECT * FROM prontuario";
-        List<Prontuario> pr = new ArrayList<Prontuario>();
-
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-
-            while(rs.next())
-            {
-                Prontuario usu = new Prontuario();
-
-                try
-                {
-                    usu.setId(rs.getLong("p_id"));
-                    usu.setData(rs.getDate("horario"));
-                    usu.setObservacoes(rs.getString("endereco"));
-
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-                pr.add(usu);
+    public List<Prontuario> listar() {
+        List<Prontuario> prontuarios = new ArrayList<>();
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT * FROM prontuario");
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                prontuarios.add(createProntuarioFromResultSet(rs));
             }
-
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection();
         }
-        finally {closeConnection();}
-        return pr;
+        return prontuarios;
+    }
+
+    private Prontuario createProntuarioFromResultSet(ResultSet rs) throws SQLException {
+        Prontuario prontuario = new Prontuario();
+        prontuario.setId(rs.getLong("p_id"));
+        prontuario.setData(rs.getDate("horario"));
+        prontuario.setObservacoes(rs.getString("observacoes"));
+        return prontuario;
     }
 }
